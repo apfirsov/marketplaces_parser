cfd0b9395f89a404895d5947865ef304ca7a0cf9
Александр Смирнов <5targazer@mail.ru>
2023-02-02 21:31:48 +0300

    added categories parser

--- parser_service/loader/categories.py
+++ parser_service/loader/categories.py
@@ -1,10 +1,23 @@
-import json
+import logging
+import sys
+from typing import Optional
 
+import requests
+from db.models import Category
+from pydantic import ValidationError
+from schemas import SourceCategory
+from settings import POSTGRES_URL
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from db.models import Category
-from settings import POSTGRES_URL
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 engine = create_engine(
     POSTGRES_URL,
@@ -15,9 +28,37 @@ engine = create_engine(
 s = Session(bind=engine)
 
 
+def _handle_response(response: list[dict]) -> list[dict]:
+    result: list[dict] = []
+    for item in response:
+        try:
+            childs: Optional[list[dict]] = item.get('childs')
+            section = SourceCategory(
+                id=item.get('id'),
+                name=item.get('name'),
+                parent_id=item.get('parent'),
+                url=item.get('url'),
+                shard=item.get('shard'),
+                query=item.get('query'),
+                childs=childs
+            )
+            if childs:
+                result.extend(_handle_response(childs))
+            section.childs = bool(childs)
+            result.append(section.dict())
+
+        except ValidationError as error:
+            logger.error(error, exc_info=True)
+
+    return result
+
+
 def load_all_items() -> None:
-    with open("loader/category.json", "r") as file:
-        objects = json.load(file)
+    catalogue_url: str = ('https://static-basket-01.wb.ru/vol0/'
+                          'data/main-menu-ru-ru-v2.json')
+    response: list[dict] = requests.get(catalogue_url).json()
+    objects: list[dict] = _handle_response(response)
+
     s.bulk_insert_mappings(
         Category,
         objects
