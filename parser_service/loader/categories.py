3b7fe80f08ab7c6b2035a5805334ca75a51b1176
femakc <femakc@yandex.ru>
2023-02-05 00:09:49 +0300

    bagfix

--- parser_service/loader/categories.py
+++ parser_service/loader/categories.py
@@ -3,21 +3,13 @@ import sys
 from typing import Optional
 
 import requests
+from constants import MAIN_MENU
 from db.models import Category
 from pydantic import ValidationError
-from loader.shemas import SourceCategory
+from schemas import SourceCategory
 from settings import POSTGRES_URL
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-
-handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
 
 engine = create_engine(
     POSTGRES_URL,
@@ -30,41 +22,38 @@ engine = create_engine(
 def _handle_response(response: list[dict]) -> list[dict]:
     result: list[dict] = []
     for item in response:
-        try:
-            childs: Optional[list[dict]] = item.get('childs')
-            section = SourceCategory(
-                id=item.get('id'),
-                name=item.get('name'),
-                parent_id=item.get('parent'),
-                url=item.get('url'),
-                shard=item.get('shard'),
-                query=item.get('query'),
-                childs=childs
-            )
-            if childs:
-                result.extend(_handle_response(childs))
-            section.childs = bool(childs)
-            result.append(section.dict())
-
-        except ValidationError as error:
-            logger.error(error, exc_info=True)
-
+        childs: Optional[list[dict]] = item.get('childs')
+        section = SourceCategory(**item)
+        if childs:
+            result.extend(_handle_response(childs))
+        result.append(section.dict())
     return result
 
 
 def load_all_items() -> None:
+
+    handler = logging.StreamHandler()
+    handler.formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+    catalogue_url: str = MAIN_MENU
     try:
-        logging.info("Запуск записи в БД categories")
-        catalogue_url: str = ('https://static-basket-01.wb.ru/vol0/'
-                          'data/main-menu-ru-ru-v2.json')
-        response: list[dict] = requests.get(catalogue_url).json()
-        objects: list[dict] = _handle_response(response)
-        s = Session(bind=engine)
-        s.query(Category).delete()
+        try:
+            response: list[dict] = requests.get(catalogue_url).json()
+            objects: list[dict] = _handle_response(response)
+        except ValidationError as error:
+            raise error
+    except Exception as error:
+        logger.error(error, exc_info=True)
+        sys.exit()
+
+    with Session(engine) as s:
         s.bulk_insert_mappings(
             Category,
             objects
         )
         s.commit()
-    except Exception as e:
-        logging.exception(f"ошибка записи в БД {e}")
